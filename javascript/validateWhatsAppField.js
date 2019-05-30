7fdd84d37aba6363e8ea04d0cf112b82b48709f5
@xouaib <gdasoft18@gmail.com>
2019-05-30 01:38:59 +0000

    get commit

---
+++ javascript/validateWhatsAppField.js
@@ -0,0 +1,10 @@
+// $(document).ready(function() {
+//     $whatsAppField = $('#inputWhatsApp');
+//     $whatsAppField.blur(function() {
+//         whatsApp = $whatsAppField.val();
+//         if (whatsApp.length < 1) return false;
+//         // if (whatsApp.length)
+//         //     alert(whatsApp.length);
+
+//     });
+// });
